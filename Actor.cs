281641e6d14cac43a01b58c5c80a8d9eba154fd2
ravis <ravissavoy49@gmail.com>
2021-10-08 11:35:00 -0500

    Added Actor class

---
+++ Actor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Actor
+    {
+        private char _icon;
+        private string _name;
+
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+    }
+}
